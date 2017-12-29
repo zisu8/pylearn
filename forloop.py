c60b444f1024632a273183c6731115e4381987b7
Florian Bens <florian.bens@gmx.net>
2017-12-29 20:50:02 +0100

    add forloop.py

---
+++ forloop.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+"""for loop example"""
+
+# for loop example 1
+
+print('\n' + "erstes for example mit einer sequence von 0 - 5" + "\n")
+
+for x in (0, 1, 2, 3, 4, 5):
+    print(x)
+
+print('\n' + "zweites for example mit range(0,10)" + "\n")
+
+# for loop example 2
+
+for x in range(0, 10):
+    print(x)
+
+print('\n' + "drittes for example mit stringsl" + "\n")
+
+for x in ("Katze", "Hund", "Maus", "Pferd", "Loewe"):
+    print(x)
